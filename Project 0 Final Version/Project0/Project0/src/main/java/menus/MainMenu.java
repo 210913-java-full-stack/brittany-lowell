1a4643735a18de09b6c090812219a144beb4ada1
BrittLowell <brittany.lowell@revature.net>
2021-10-05 17:26:20 -0600

    accidently moved folder to a different directory

---
+++ Project 0 Final Version/Project0/Project0/src/main/java/menus/MainMenu.java
@@ -0,0 +1,81 @@
+package menus;
+
+import exceptions.InvalidConsoleResponse;
+import utils.Connections;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+/**
+ * This class displays the main menu and sends the user to the login menu, register menu, or it ends the app depending
+ * on which number the user enters.
+ */
+public class MainMenu {
+    private boolean running;
+    Connection conn = Connections.getConnection(); //Created the connection in this class instead of the Driver class.
+    Scanner input = new Scanner(System.in); //Scanner object bound to System.in, the console input
+    public MainMenu() {
+        running = true;
+    }
+
+    public Connection getConn(){
+        return conn;
+    }
+
+    public Scanner getScanner(){
+        return input;
+    }
+
+    /*
+    I did not want to make another class, so I am calling methods from the other classes based on their responses in
+    this switch statement. This was how I chose to avoid getting stuck in an infinite loop.
+     */
+    public void viewMainMenu() {
+        System.out.println("Homepage\nPlease choose an option below:\n" +
+                "1) Login\n2) Register\n3) Exit App");
+        String inNum = input.nextLine(); //Takes the user input on the next line and stores it in inNum.
+        //This if, then, else statement stops the user from inputting invalid values.
+        if (inNum.equals("1") || inNum.equals("2") || inNum.equals("3")) {
+
+            switch (inNum) {
+                case "1":
+                    //The login class contains a "logout" option that returns the user to the main menu.
+                    Login login = new Login();
+                    login.runLogin(); //Abstracts the inner workings of the Login class
+                    return;
+                case "2":
+                    //go to the register screen
+                    Register register = new Register();
+                    register.runRegister();
+                    return;
+                case "3":
+                    //Exiting the app
+                    setRunning(false);
+                    try {
+                        conn.close();
+                    } catch (SQLException throwables) {
+                        throwables.printStackTrace();
+                    }
+                    break; //Probably not necessary, but I don't feel comfortable leaving this open to fall through.
+            }
+        } else {
+            try {
+                throw new InvalidConsoleResponse("This menu only accepts the responses: 1, 2, or 3.");
+            } catch (InvalidConsoleResponse invalidConsoleResponse) {
+                invalidConsoleResponse.printStackTrace();
+                System.out.println("Please type 1, 2, or 3 as an integer.\n");
+            }
+        }
+
+    }
+
+    //Boolean version of a getter
+    public boolean isRunning() {
+        return running;
+    }
+
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
+}
---
+++ Project 0 Final Version/Project0/Project0/src/main/java/models/UserAccounts.java
@@ -0,0 +1,57 @@
+package models;
+
+import daos.AccountsDAO;
+
+/**
+ * This model class allows the user to access the information stored in the user_account table.
+ * The combination of the public methods and the private variables restricts the access the user has to these
+ * variables. They have to go through the getter and setter methods to interact with these variables.
+ */
+public class UserAccounts {
+    private int id;
+    private int account_id;
+    private int user_id;
+
+    public UserAccounts() {
+    }
+
+    public UserAccounts(int account_id) {
+        this.account_id = account_id;
+    }
+
+    public UserAccounts(int id, int account_id) {
+        this.id = id;
+        this.account_id = account_id;
+    }
+
+    public UserAccounts(int id, int account_id, int user_id) {
+        this.id = id;
+        this.account_id = account_id;
+        this.user_id = user_id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAccount_id() {
+        return account_id;
+    }
+
+    public void setAccount_id(int account_id) {
+        this.account_id = account_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+}
